b000e675ec640e77ba48c81665aa6e661ee9a221
mahrmlolo@gmail.com <mahrmlolo@gmail.com>
2022-11-27 20:01:00 +0200

    null

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,7 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
       
-        title: const Text(''),
+        title: const Text('master branch'),
       ),
       body: Center(
 
